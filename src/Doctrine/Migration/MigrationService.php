ed992a9606e61b22e00074820beac0ae67ef6bee
Jan Nedbal <janedbal@gmail.com>
2022-01-12 16:04:41 +0100

    Minor improvements (#5)
    * Minor improvements
    
    * Fix cs

--- src/Doctrine/Migration/MigrationService.php
+++ src/Doctrine/Migration/MigrationService.php
@@ -15,6 +15,7 @@ use function ksort;
 use function sprintf;
 use function str_replace;
 use function strlen;
+use function trim;
 
 class MigrationService
 {
@@ -45,7 +46,7 @@ class MigrationService
     {
         $this->connection = $connection;
         $this->migrationsDir = $migrationsDir;
-        $this->migrationClassNamespace = $migrationClassNamespace;
+        $this->migrationClassNamespace = trim($migrationClassNamespace, '\\');
         $this->migrationClassPrefix = $migrationClassPrefix;
         $this->includeDropTableInDatabaseSync = $includeDropTableInDatabaseSync;
         $this->templateFilePath = $templateFilePath;
@@ -186,7 +187,7 @@ class MigrationService
         $template = str_replace('%statements%', implode("\n" . $this->templateIndent, $statements), $template);
 
         $filePath = $migrationsDir . '/' . $migrationClassPrefix . $version . '.php';
-        FileSystem::createDir($migrationsDir);
+        FileSystem::createDir($migrationsDir, 655);
         FileSystem::write($filePath, $template);
 
         return new MigrationFile($filePath, $version);
