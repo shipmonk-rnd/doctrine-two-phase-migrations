4d23082442d9eb5301e7eec733841abceff6c44d
Jan Nedbal <janedbal@gmail.com>
2023-02-16 09:29:22 +0100

    Transactional marked by interface, not method

---
+++ src/Doctrine/Migration/TransactionalMigration.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonk\Doctrine\Migration;
+
+/**
+ * Marking interface causing each phase to be executed in transaction
+ * - beware that many databases like MySQL does not support DDL operations within transaction
+ */
+interface TransactionalMigration
+{
+
+}
