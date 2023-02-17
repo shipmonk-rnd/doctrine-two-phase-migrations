8132b19e46035521907c86f97e46e94de4198b8e
Jan Nedbal <janedbal@gmail.com>
2023-02-17 11:29:36 +0100

    TransactionalMigration extends Migration

--- src/Doctrine/Migration/TransactionalMigration.php
+++ src/Doctrine/Migration/TransactionalMigration.php
@@ -6,7 +6,7 @@ namespace ShipMonk\Doctrine\Migration;
  * Marking interface causing each phase to be executed in transaction
  * - beware that many databases like MySQL does not support DDL operations within transaction
  */
-interface TransactionalMigration
+interface TransactionalMigration extends Migration
 {
 
 }
