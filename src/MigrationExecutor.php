8e5c549a1054413d11bd598e3dffda97f0f106a6
Vlasta Neubauer <cz.paranoiq@gmail.com>
2024-07-03 11:30:38 +0200

    Update signature

--- src/MigrationExecutor.php
+++ src/MigrationExecutor.php
@@ -2,7 +2,9 @@
 
 namespace ShipMonk\Doctrine\Migration;
 
+use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
 use Doctrine\DBAL\Result;
 use Doctrine\DBAL\Types\Type;
 
@@ -11,7 +13,7 @@ interface MigrationExecutor
 
     /**
      * @param list<mixed>|array<string, mixed> $params
-     * @param array<int|string, int|string|Type|null> $types
+     * @param array<non-negative-int|string, string|Type|ParameterType|ArrayParameterType> $types
      */
     public function executeQuery(string $statement, array $params = [], array $types = []): Result;
 
