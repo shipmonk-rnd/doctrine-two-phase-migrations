6df0a6436835670453391804e47a27211ffa80aa
Vlasta Neubauer <cz.paranoiq@gmail.com>
2024-08-19 14:49:44 +0200

    Support for Doctrine/DBAL 4 (#94)
    * Allow DBAL 4
    
    * Update signature

--- src/MigrationDefaultExecutor.php
+++ src/MigrationDefaultExecutor.php
@@ -2,7 +2,9 @@
 
 namespace ShipMonk\Doctrine\Migration;
 
+use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
 use Doctrine\DBAL\Result;
 use Doctrine\DBAL\Types\Type;
 
@@ -20,7 +22,7 @@ class MigrationDefaultExecutor implements MigrationExecutor
 
     /**
      * @param list<mixed>|array<string, mixed> $params
-     * @param array<int|string, int|string|Type|null> $types
+     * @param array<non-negative-int|string, string|Type|ParameterType|ArrayParameterType> $types
      */
     public function executeQuery(string $statement, array $params = [], array $types = []): Result
     {
